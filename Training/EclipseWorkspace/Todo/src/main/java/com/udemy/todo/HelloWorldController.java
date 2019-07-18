04364d0c8dfa42f6581962766f8c6089996ddc6a
sean sulham <sean.sulham@hotmail.com>
2019-07-17 23:03:29 -0700

    Added CrossOrigin Annotation in Spring Boot

--- Training/EclipseWorkspace/Todo/src/main/java/com/udemy/todo/HelloWorldController.java
+++ Training/EclipseWorkspace/Todo/src/main/java/com/udemy/todo/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.udemy.todo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 	
@@ -16,7 +18,7 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World Bean");
+		return new HelloWorldBean("Hello World Bean - Changed...NOW");
 	}
 	
 	@GetMapping(path = "hello-world-bean/path-variable/{name}")
