265192b051bca322b3e1955a595e8b1906dc76b7
sean sulham <sean.sulham@hotmail.com>
2019-09-06 22:14:56 -0700

    Adding Update Functionality

--- Training/AngularWorkspace/Todo/src/app/services/data/todo-data.service.ts
+++ Training/AngularWorkspace/Todo/src/app/services/data/todo-data.service.ts
@@ -19,6 +19,10 @@ export class TodoDataService {
   //   return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`);
   // }
 
+  retrieveTodo(username, id) {
+    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+  }
+
   deleteTodo(username, id) {
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
   }
--- Training/EclipseWorkspace/Todo/src/main/java/com/udemy/todo/todo/TodoResource.java
+++ Training/EclipseWorkspace/Todo/src/main/java/com/udemy/todo/todo/TodoResource.java
@@ -3,11 +3,14 @@ package com.udemy.todo.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins="http://localhost:4200")
@@ -17,9 +20,25 @@ public class TodoResource {
 	@Autowired
 	private TodoHardcodedService todoService;
 	
+	// GET All Todos
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
+	}
+	
+	// GET Todo
+	@GetMapping("/users/{username}/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+		return todoService.findById(id);
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username, 
+			@PathVariable long id, 
+			@RequestBody Todo todo) {
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
 	}
 	
 	// DELETE ("/users/{username}/todos/{id}")
